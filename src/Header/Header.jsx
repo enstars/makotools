17148a0f5e2f84c8bac1a93b43dfd230c2d6ee32
Midori Yang <midori@joinmonument.com>
2021-10-31 15:45:18 -0400

    set up AuthProvider context

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,22 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../services/auth";
 import "./Header.scss";
 
 function Header() {
+    const { user } = useAuth();
     return (
         <header className="es-header">
             <div className="es-header__content">
-                <div className="es-header__branding">
-                    Ensemble Square
-                </div>
+                <div className="es-header__branding">Ensemble Square</div>
                 <div className="es-header__menu">
                     <nav className="es-header__links">
                         <NavLink to="/cards">Cards</NavLink>
                         <NavLink to="/events">Events</NavLink>
                         <NavLink to="/stories">Stories</NavLink>
                     </nav>
-                    <div className="es-header__account">
-                        {/* {user ? user.email : ""} */}
-                    </div>
+                    <div className="es-header__account">{user?.email}</div>
                 </div>
             </div>
         </header>
