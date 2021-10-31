48585fde144b8f801082ae97beff70965c52c360
Midori Yang <midori@joinmonument.com>
2021-10-31 11:34:40 -0400

    test out sass

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,3 +1,5 @@
+import "./Header.scss";
+
 function Header() {
     return (
         <header>
