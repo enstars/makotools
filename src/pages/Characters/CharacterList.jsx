59bc9e14234170f37d79068be6d379f100b47209
Midori Yang <midori@joinmonument.com>
2021-11-04 11:24:14 -0400

    refactor with react-query

--- src/pages/Characters/CharacterList.jsx
+++ src/pages/Characters/CharacterList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useQueries } from "react-query";
 import Multiselect from "multiselect-react-dropdown";
 import { getData, getB2File } from "../../services/ensquare";
 import "./CharacterList.scss";
@@ -8,6 +9,46 @@ function CharacterList() {
     const [listCharacters, setListCharacters] = useState([]);
     const [filterList, setFilterList] = useState([]);
     const [filterChoice, setFilterChoice] = useState([]);
+    const characterListQuery = useQueries([
+        {
+            queryKey: ["characters"],
+            queryFn: () => getData("characters"),
+        },
+        {
+            queryKey: ["unit_to_characters"],
+            queryFn: () => getData("unit_to_characters"),
+        },
+        {
+            queryKey: ["units"],
+            queryFn: () => getData("units"),
+        },
+    ]);
+    const hasAllData = characterListQuery.reduce(
+        (hasData, query) => hasData && query.data,
+        true,
+    );
+
+    useEffect(() => {
+        if (hasAllData) {
+            const [charactersQuery, unitToCharactersQuery, unitsQuery] =
+                characterListQuery;
+            let charactersWithUnits = unitToCharactersQuery.data;
+            if (filterList.length > 0) {
+                charactersWithUnits = charactersWithUnits.filter((character) =>
+                    filterChoice.map((a) => a.id).includes(character.unit_id),
+                );
+            }
+            const characters = charactersWithUnits.map(
+                (charaUnit) =>
+                    charactersQuery.data.filter(
+                        (chara) => chara.id === charaUnit.character_id,
+                    )[0],
+            );
+            setListCharacters(characters);
+
+            setFilterList(unitsQuery.data);
+        }
+    }, [hasAllData]);
 
     const unitFilter = {
         onSelect(selectedList, selectedItem) {
@@ -20,41 +61,10 @@ function CharacterList() {
         },
     };
 
-    useEffect(() => {
-
-    }, []);
-
-    useEffect(() => {
-        Promise.allSettled([
-            getData("characters"),
-            getData("unit_to_characters"),
-            getData("units"),
-        ])
-            .then((data) => {
-                let charactersWithUnits = data[1].value;
-                if (filterList.length > 0) {
-                    charactersWithUnits = charactersWithUnits.filter((character) => filterChoice.map((a) => a.id).includes(character.unit_id));
-                }
-                const characters = charactersWithUnits.map((charaUnit) => (
-                    data[0].value.filter((chara) => chara.id === charaUnit.character_id)[0]
-                ));
-                const charactersElement = characters.map((character) => (
-                    <Link to={`/characters/${character.id}`} className="es-characterList__character">
-                        {/* <img src={getB2File(`icon/character_sd_square1_${character.id}.png`)} alt={character.first_name} /> */}
-                        <img src={getB2File(`render/character_full1_${character.id}.png`)} alt={character.first_name} />
-                        <div className="es-characterList__info">
-                            <span>
-                                {character.last_name}
-                                {character.first_name}
-                            </span>
-                        </div>
-                    </Link>
-                ));
-                setListCharacters(charactersElement);
-
-                setFilterList(data[2].value);
-            });
-    }, []);
+    if (!hasAllData) {
+        // This should probably be a more friendly loading state lol
+        return null;
+    }
 
     return (
         <div>
@@ -65,7 +75,28 @@ function CharacterList() {
                 onRemove={unitFilter.onRemove} // Function will trigger on remove event
                 displayValue="name"
             />
-            <div className="es-characterList">{listCharacters}</div>
+            <div className="es-characterList">
+                {listCharacters.map((character) => (
+                    <Link
+                        to={`/characters/${character.id}`}
+                        className="es-characterList__character"
+                    >
+                        {/* <img src={getB2File(`icon/character_sd_square1_${character.id}.png`)} alt={character.first_name} /> */}
+                        <img
+                            src={getB2File(
+                                `render/character_full1_${character.id}.png`,
+                            )}
+                            alt={character.first_name}
+                        />
+                        <div className="es-characterList__info">
+                            <span>
+                                {character.last_name}
+                                {character.first_name}
+                            </span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
