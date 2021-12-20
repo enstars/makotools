332dc9b0aa40073b16bd7677a24bc81c79e1931e
yuuukun <58155530+yuuukun@users.noreply.github.com>
2021-12-20 23:34:07 +0700

    adjusted page padding

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 const contentWidth = 900;
 const MainContent = styled.main`
-    padding: 10px;
+  padding: 10px;
+
+  @media only screen and (min-width: 600px) {
+    padding: 20px;
+  }
 `;
 function Main({ fullWidth, children }) {
-    if (fullWidth) return <MainContent>{children}</MainContent>;
-    return (
-        <MainContent style={{ maxWidth: contentWidth, margin: "auto" }}>
-            {children}
-        </MainContent>
-    );
+  if (fullWidth) return <MainContent>{children}</MainContent>;
+  return (
+    <MainContent style={{ maxWidth: contentWidth, margin: "auto" }}>
+      {children}
+    </MainContent>
+  );
 }
 export default Main;
