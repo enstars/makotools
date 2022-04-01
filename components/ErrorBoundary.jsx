c0f33446474837cee3f548f945ed7709e0841140
yuuukun <58155530+yuuukun@users.noreply.github.com>
2022-04-02 01:26:05 +0700

    whoops

--- components/ErrorBoundary.jsx
+++ components/ErrorBoundary.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-    static getDerivedStateFromError() {
-        return { hasError: true };
-    }
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
 
-    render() {
-        const { hasError } = this.state;
-        const { children } = this.props;
-        if (hasError) {
-            // console.log(this);
-            return "Something went wrong";
-        }
-        return children;
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      // console.log(this);
+      return "Something went wrong";
     }
+    return children;
+  }
 }
 
 export default ErrorBoundary;
