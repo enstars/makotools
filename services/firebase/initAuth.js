586a8ab1b425f5aa90b9fdc4732c8d470b37716d
yuuukun <58155530+yuuukun@users.noreply.github.com>
2022-07-14 22:47:40 +0700

    midway in migrating to services/firebase

--- services/firebase/initAuth.js
+++ services/firebase/initAuth.js
@@ -1,8 +1,43 @@
 // ./initAuth.js
 import { init } from "next-firebase-auth";
 
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  GoogleAuthProvider,
+  signOut,
+  getRedirectResult,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  serverTimestamp,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+
+import "firebase/compat/auth";
+
 const parseKey = (key) => {
   return key?.replace(/\\n/g, "\n") || undefined;
+};
+
+// Config & Initialization
+const firebaseConfig = {
+  apiKey: "AIzaSyA530zKJVq_vi56gzta4J_jGWIxgCIJg2k",
+  authDomain: "ensemble-square.firebaseapp.com",
+  projectId: "ensemble-square",
+  storageBucket: "ensemble-square.appspot.com",
+  messagingSenderId: "940403567905",
+  appId: "1:940403567905:web:51d666cacd10e979cb2260",
+  measurementId: "G-L1FLXJKQC5",
 };
 
 const initAuth = () => {
@@ -32,12 +67,7 @@ const initAuth = () => {
     },
     // Use application default credentials (takes precedence over firebaseAdminInitConfig if set)
     // useFirebaseAdminDefaultCredential: true,
-    firebaseClientInitConfig: {
-      apiKey: "AIzaSyA530zKJVq_vi56gzta4J_jGWIxgCIJg2k",
-      authDomain: "ensemble-square.firebaseapp.com",
-      //   databaseURL: "https://my-example-app.firebaseio.com",
-      projectId: "ensemble-square",
-    },
+    firebaseClientInitConfig: firebaseConfig,
     cookies: {
       name: "MakoTools", // required
       // Keys are required unless you set `signed` to `false`.
@@ -64,3 +94,98 @@ const initAuth = () => {
 };
 
 export default initAuth;
+
+// const firebaseApp = initializeApp(firebaseConfig);
+
+// Authentication
+const clientAuth = getAuth();
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const appSignInWithGoogle = () =>
+  signInWithRedirect(clientAuth, provider);
+export const appSignOut = () => signOut(clientAuth);
+
+export const appSignInWithEmailAndPassword = (
+  email,
+  password,
+  callback = () => {}
+) =>
+  signInWithEmailAndPassword(clientAuth, email, password)
+    .then((result) => {
+      //   syncFirestoreUserData(result.user);
+    })
+    .catch((error) => {
+      callback({ status: "error", error });
+    });
+export const appSignUpWithEmailAndPassword = (
+  email,
+  password,
+  userInfo,
+  callback = () => {}
+) => {
+  createUserWithEmailAndPassword(clientAuth, email, password)
+    .then((result) => {
+      //   syncFirestoreUserData(result.user, callback, userInfo);
+      // console.log(0);
+    })
+    .catch((error) => {
+      // console.log(callback);
+      callback({ status: "error", error });
+    });
+};
+
+// Firestore Database
+const db = getFirestore();
+
+// getRedirectResult(clientAuth)
+//   .then((result) => {
+//     if (result) {
+//       const { user } = result;
+//       // console.log(user);
+//       syncFirestoreUserData(user);
+//     }
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//   });
+
+// function syncFirestoreUserData(user, callback = () => {}, userInfo = {}) {
+//   // console.log(user);
+//   setFirestoreUserData(
+//     {
+//       ...userInfo,
+//       // googleUser: JSON.stringify(user),
+//       user: JSON.stringify(user),
+//       // i actually have no idea if this is safe. but this should be only public info so
+//       lastLogin: serverTimestamp(),
+//     },
+//     user.uid
+//   );
+// }
+
+function setFirestoreUserData(
+  data,
+  uid = clientAuth?.currentUser?.uid || null
+) {
+  if (uid) setDoc(doc(db, "users", uid), data, { merge: true });
+}
+
+async function getFirestoreUserData(uid = clientAuth.currentUser.uid) {
+  const docSnap = await getDoc(doc(db, "users", uid));
+
+  if (docSnap.exists()) {
+    const data = docSnap.data();
+    return data;
+  }
+  return null;
+}
+
+async function validateUsernameDb(username) {
+  const q = query(collection(db, "users"), where("username", "==", username));
+  const querySnap = await getDocs(q);
+  const usernameValid = !!!querySnap.size;
+  return usernameValid;
+}
+
+export { setFirestoreUserData, getFirestoreUserData, validateUsernameDb };
