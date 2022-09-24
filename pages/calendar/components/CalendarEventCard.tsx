cab8849033f3ad266e7337af1b2fbd343641669f
neeneemi <neeneemille@gmail.com>
2022-09-24 10:31:34 -0500

    refactor: :lipstick: updated style for calendar
    made the calendar more simple

--- pages/calendar/components/CalendarEventCard.tsx
+++ pages/calendar/components/CalendarEventCard.tsx
@@ -16,105 +16,46 @@ import { twoStarIDs } from "../../../data/characterIDtoCardID";
 
 function CalendarEventCard({ ...props }) {
   const useStyles = createStyles((theme, _params, getRef) => ({
-    birthdayCardImage: {
-      ref: getRef("image"),
-      visibility: "hidden",
-      height: "0vh",
-      marginTop: "10%",
-      transition: "height 0.5s, visibility 0.5s ease-in, border-radius 0.5s",
-    },
-
-    cardImageOverlay: {
-      position: "absolute",
-      width: "110%",
-      height: "15vh",
-      margin: "auto",
-      marginLeft: "0.5vw",
-      marginTop: "-7vh",
-      background: `linear-gradient(${
-        theme.colorScheme === "dark"
-          ? theme.colors.blue[7]
-          : theme.colors.blue[4]
-      } 25%, transparent)`,
-      zIndex: 1,
-    },
-
     eventCard: {
-      padding: "3px 0px 6px 0px !important",
-      marginTop: "3px",
-      textAlign: "left",
-      color: theme.colors.gray[0],
-      borderRadius: 0,
-      transition: "border-radius 0.5s",
+      fontSize: "12px",
       "&:hover": {
-        borderRadius: theme.radius.md,
         cursor: "pointer",
       },
-      [`&:hover .${getRef("image")}`]: {
-        visibility: "visible",
-        height: "10vh",
-        borderRadius: theme.radius.md,
-      },
-      background:
-        theme.colorScheme === "dark"
-          ? theme.colors.blue[7]
-          : theme.colors.blue[4],
-    },
-
-    eventCardText: {
-      padding: "0px 3px",
-      display: "flex",
-      flexFlow: "row wrap",
-      alignItems: "flex-start",
-      justifyContent: "space-evenly",
-      textAlign: "center",
-      maxWidth: "100%",
-      margin: "auto",
-      zIndex: 3,
     },
   }));
 
   const { event } = props;
   const { classes } = useStyles();
   return (
-    <Card className={classes.eventCard}>
-      <Link
-        href={
-          event.type === "birthday" || event.type === "feature scout"
-            ? `/characters/${event.id}`
-            : `/events/${event.id}`
+    <Link
+      href={
+        event.type === "birthday" || event.type === "feature scout"
+          ? `/characters/${event.id}`
+          : `/events/${event.id}`
+      }
+      passHref
+    >
+      <Badge
+        fullWidth
+        variant="dot"
+        color={
+          event.type === "anniversary"
+            ? "yellow"
+            : event.type === "birthday"
+            ? "cyan"
+            : event.status === "start"
+            ? "lime"
+            : "pink"
         }
+        className={classes.eventCard}
       >
-        <Card.Section component="a" className={classes.eventCardText}>
-          <Text sx={{ verticalAlign: "center", zIndex: 5 }}>
-            {event.type === "birthday"
-              ? event.name.split(" ")[0] + "'s birthday"
-              : event.type === "anniversary"
-              ? event.name
-              : event.status.toUpperCase() + ": " + event.short_name}
-          </Text>
-        </Card.Section>
-      </Link>
-      <Card.Section className={classes.birthdayCardImage}>
-        <div className={classes.cardImageOverlay}></div>
-        <Image
-          src={getB2File(
-            `assets/card_still_full1_${
-              event.type === "birthday"
-                ? event.render_id + "_normal"
-                : event.render_id + "_evolution"
-            }.webp`
-          )}
-          alt={event.name}
-          width={500}
-          sx={{
-            position: "absolute",
-            left: "-85%",
-            top: "5%",
-          }}
-        />
-      </Card.Section>
-    </Card>
+        {event.type === "birthday"
+          ? event.name.split(" ")[0] + "'s birthday"
+          : event.type === "anniversary"
+          ? event.name
+          : event.status + ": " + event.short_name}
+      </Badge>
+    </Link>
   );
 }
 
