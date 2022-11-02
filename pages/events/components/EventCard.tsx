807c99e9a932ba27184c78fbd1e94a91f6e8f38f
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-11-02 22:01:28 +0700

    refactor: 💄 event card touchup

--- pages/events/components/EventCard.tsx
+++ pages/events/components/EventCard.tsx
@@ -6,8 +6,9 @@ import {
   Text,
   Badge,
   Group,
-  Blockquote,
   Tooltip,
+  Stack,
+  ThemeIcon,
 } from "@mantine/core";
 import Link from "next/link";
 import { IconArrowsShuffle2, IconBus, IconDiamond } from "@tabler/icons";
@@ -17,28 +18,34 @@ import { GameEvent, GameUnit } from "types/game";
 import { useDayjs } from "services/libraries/dayjs";
 import IconEnstars from "components/core/IconEnstars";
 
-const useStyles = createStyles((theme, _params, getRef) => ({
+const useStyles = createStyles((theme) => ({
   eventCard: {
     display: "flex",
     flexFlow: "row wrap",
     width: "100%",
     margin: "auto",
-    // marginTop: "2vh",
+    overflow: "hidden",
+    marginBottom: theme.spacing.xs,
   },
   eventInfo: {
     position: "relative",
     flex: "2 1 60%",
-    padding: "10px 10px 10px 20px",
     minWidth: 200,
-    ["@media (max-width: 768px)"]: {
-      width: "100%",
+    rowGap: 0,
+    columnGap: theme.spacing.md,
+  },
+  eventInfoText: {
+    "&&&&&": {
+      flex: "2 1 60%",
+      minWidth: 200,
+      maxWidth: 500,
     },
   },
-  eventDate: {
-    color:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[2]
-        : theme.colors.gray[6],
+  eventInfoDates: {
+    "&&&&&": {
+      flex: "1 1 30%",
+      minWidth: 200,
+    },
   },
   eventSummary: {
     fontSize: "11pt",
@@ -46,23 +53,20 @@ const useStyles = createStyles((theme, _params, getRef) => ({
   },
 }));
 
-function EventCard({
-  event,
-  units,
-  locale,
-}: {
-  event: GameEvent;
-  units: GameUnit[];
-  locale: string | undefined;
-}) {
+function EventCard({ event, units }: { event: GameEvent; units: GameUnit[] }) {
   const { classes } = useStyles();
   const { dayjs } = useDayjs();
+
+  const isEstimatedDate = dayjs(event.start_date).isAfter(dayjs());
+  const isOngoing = dayjs().isBetween(
+    dayjs(event.start_date),
+    dayjs(event.end_date)
+  );
 
   return (
     <Paper
       component={Link}
       href={`/events/${event.event_id}`}
-      shadow="xs"
       withBorder
       className={classes.eventCard}
     >
@@ -70,105 +74,114 @@ function EventCard({
         <Picture
           alt={event.name}
           srcB2={`assets/card_still_full1_${event.banner_id}_evolution.png`}
-          radius="sm"
           sx={(theme) => ({
             height: "100%",
             minHeight: 150,
           })}
         />
       </Box>
-      <Box className={classes.eventInfo}>
-        <Group noWrap>
-          <Title order={3} sx={{ width: "80%" }}>
+      <Group className={classes.eventInfo} align="start" px="md" py="sm">
+        <Box className={classes.eventInfoText}>
+          <Title order={3}>
             {event.name[0]}
-          </Title>
-          <Badge
-            variant="filled"
-            color={
-              event.type === "song"
-                ? "grape"
-                : event.type === "shuffle"
-                ? "blue"
-                : "teal"
-            }
-            leftSection={
-              <Box mt={4}>
-                {event.type === "song" ? (
-                  <IconDiamond size={12} strokeWidth={3} />
-                ) : event.type === "shuffle" ? (
-                  <IconArrowsShuffle2 size={12} strokeWidth={3} />
-                ) : (
-                  <IconBus size={12} strokeWidth={3} />
-                )}
-              </Box>
-            }
-          >
-            {event.type}
-          </Badge>
-        </Group>
-        <Group>
-          <Text size="sm" className={classes.eventDate}>
-            {dayjs(event.start_date).format("lll")}
-            {" - "}
-            {dayjs(event.end_date).format("lll z")}
-          </Text>
-          {dayjs(event.start_date).isAfter(dayjs()) && (
-            <Tooltip
-              multiline
-              width={250}
-              label="This event has not occured yet and this date range will be updated when official dates are provided."
-              position="bottom"
-              withArrow
-            >
-              <Badge color="red" variant="dot">
-                Estimate
+            {isOngoing && (
+              <Badge
+                ml="xs"
+                color="yellow"
+                variant="filled"
+                size="md"
+                sx={{ verticalAlign: 3 }}
+              >
+                Ongoing
               </Badge>
-            </Tooltip>
-          )}
-          {dayjs().isBetween(
-            dayjs(event.start_date),
-            dayjs(event.end_date)
-          ) && (
-            <Badge color="yellow" variant="dot">
-              Ongoing
-            </Badge>
-          )}
-        </Group>
+            )}
+          </Title>
 
-        <Blockquote className={classes.eventSummary}>
-          {event.intro_lines?.[0] || "Event description to be announced soon."}
-        </Blockquote>
-        <Box>
-          {units.length > 0 && (
-            <Group
-              sx={{
-                position: "absolute",
-                bottom: 0,
-                marginBottom: "1vh",
-                paddingTop: "5px",
-              }}
-            >
+          <Text className={classes.eventSummary}>
+            {event.intro_lines?.[0] ||
+              "Event description to be announced soon."}
+          </Text>
+        </Box>
+        <Box className={classes.eventInfoDates}>
+          <Stack spacing={0}>
+            <Group spacing="xs">
+              <Badge
+                size="lg"
+                variant="filled"
+                color={
+                  event.type === "song"
+                    ? "grape"
+                    : event.type === "shuffle"
+                    ? "blue"
+                    : "teal"
+                }
+                leftSection={
+                  <Box mt={4}>
+                    {event.type === "song" ? (
+                      <IconDiamond size={14} strokeWidth={3} />
+                    ) : event.type === "shuffle" ? (
+                      <IconArrowsShuffle2 size={14} strokeWidth={3} />
+                    ) : (
+                      <IconBus size={14} strokeWidth={3} />
+                    )}
+                  </Box>
+                }
+              >
+                {event.type}
+              </Badge>
+
               {units.map((unit) => (
-                <Badge
+                <ThemeIcon
                   key={unit.id}
-                  // size="xs"
-                  // weight={600}
                   color={unit.image_color}
                   sx={(theme) => ({
-                    background: `${unit.image_color}44`,
-                    color: unit.image_color,
-                    padding: "2px 8px",
                     borderRadius: theme.radius.lg,
                   })}
-                  leftSection={<IconEnstars unit={unit.id} size={10} />}
                 >
-                  {unit.name[0]}
-                </Badge>
+                  <IconEnstars unit={unit.id} size={14} />
+                </ThemeIcon>
               ))}
             </Group>
-          )}
+            <Group
+              sx={(theme) => ({
+                rowGap: theme.spacing.xs,
+                columnGap: theme.spacing.md,
+              })}
+              mt="xs"
+            >
+              <Box sx={{ flex: "1 1 0", minWidth: 185 }}>
+                <Text size="xs" color="dimmed" weight={700}>
+                  Start ({dayjs(event.start_date).format("z")})
+                </Text>
+                <Text weight={500}>
+                  {dayjs(event.start_date).format("lll")}
+                </Text>
+              </Box>
+              <Box sx={{ flex: "1 1 0", minWidth: 185 }}>
+                <Text size="xs" color="dimmed" weight={700}>
+                  End ({dayjs(event.end_date).format("z")})
+                </Text>
+                <Text weight={500}>{dayjs(event.end_date).format("lll")}</Text>
+              </Box>
+            </Group>
+
+            {isEstimatedDate && (
+              <Tooltip
+                multiline
+                width={250}
+                label="This event is not available on the global server yet."
+                position="bottom-start"
+                withArrow
+                p="sm"
+              >
+                <Text color="dimmed" size="xs">
+                  Dates are estimates
+                </Text>
+              </Tooltip>
+            )}
+          </Stack>
         </Box>
-      </Box>
+      </Group>
     </Paper>
   );
 }
