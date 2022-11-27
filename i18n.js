e98a421af7c48b331488893e229067ff58ff8963
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-11-27 13:08:32 +0700

    refactor: 🌐 i18n files are now loaded from tl.stars.ensemble.moe

--- i18n.js
+++ i18n.js
@@ -1,26 +1,26 @@
 module.exports = {
   locales: [
     // Official game languages
-    "en", // English
-    "ja", // Japanese
-    "zh", // Standard Mandarin / Simplified
-    "zh-TW", // Taiwanese Mandarin / Traditional
-    "ko", // Korean
+    "en", //      English
+    "ja", //      Japanese
+    "zh", //      Standard Mandarin / Simplified
+    "zh-TW", //   Taiwanese Mandarin / Traditional
+    "ko", //      Korean
 
     //  MakoTools statistics
-    "id", // Indonesian
-    "pt-BR", // Brazilian Portugese
-    "es", // Spanish
-    "th", // Thai
-    "ru", // Russian
-    // "it", // Italian
-    // "fil", // Filipino
-    // "ms", // Malaysian
-    // "vi", // Vietnamese
-    // "de", // German
-    // "pol", // Polish
-    // "fr", // French
-    // "pt", // Portugese
+    "id", //      Indonesian
+    "pt-BR", //   Brazilian Portugese
+    "es", //      Spanish
+    "th", //      Thai
+    "ru", //      Russian
+    // "it",  //  Italian
+    // "fil", //  Filipino
+    // "ms",  //  Malaysian
+    // "vi",  //  Vietnamese
+    // "de",  //  German
+    // "pol", //  Polish
+    // "fr",  //  French
+    // "pt",  //  Portugese
   ],
   defaultLocale: "en",
   pages: {
@@ -28,7 +28,11 @@ module.exports = {
     "/": ["home"],
     // "/about": ["about"],
   },
-  // loadLocaleFrom: async (lang, ns) => {
-  //   return import(`./locales/${lang}/${ns}.json`).then((m) => m.default);
-  // },
+  loadLocaleFrom: async (lang, ns) => {
+    if (process.env.NODE_ENV === "development")
+      return import(`./locales/${lang}/${ns}.json`).then((m) => m.default);
+    return fetch(`https://tl.stars.ensemble.moe/${lang}/${ns}.json`).then((r) =>
+      r.json()
+    );
+  },
 };
