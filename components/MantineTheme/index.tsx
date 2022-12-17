a2d2b03a6e9e9534fe90ca025a5ab3a9f5e5ed65
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-12-18 01:43:21 +0700

    refactor: ♻️ moved mantine theme element to components folder

---
+++ components/MantineTheme/index.tsx
@@ -0,0 +1,95 @@
+import { MantineProvider } from "@mantine/core";
+import { ReactNode } from "react";
+
+import { emotionCache } from "services/libraries/emotion";
+
+function MantineTheme({
+  colorScheme,
+  children,
+  setAppColorScheme,
+  toggleAppColorScheme,
+}: {
+  colorScheme: "light" | "dark";
+  children: ReactNode;
+  setAppColorScheme: (c: any) => void;
+  toggleAppColorScheme: () => void;
+}) {
+  return (
+    <MantineProvider
+      emotionCache={emotionCache}
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme,
+        components: {
+          NavLink: {
+            styles: (theme) => ({
+              root: {
+                "& > *:last-child": {
+                  margin: 0,
+                },
+              },
+            }),
+          },
+        },
+        colors: {
+          dark: [
+            "#D3D6E0",
+            "#AAB1C2",
+            "#8E97AD",
+            "#5F6982",
+            "#3A4259",
+            "#2C3347",
+            "#212736",
+            "#191C27",
+            "#171921",
+            "#12141C",
+          ],
+          lightblue: [
+            "#e7f5ff",
+            "#d0ebff",
+            "#a5d8ff",
+            "#74c0fc",
+            "#4dabf7",
+            "#339af0",
+            "#228be6",
+            "#1c7ed6",
+            "#1971c2",
+            "#1864ab",
+          ],
+          blue: [
+            "#edf2ff",
+            "#dbe4ff",
+            "#bac8ff",
+            "#91a7ff",
+            "#748ffc",
+            "#5c7cfa",
+            "#4c6ef5",
+            "#4263eb",
+            "#3b5bdb",
+            "#364fc7",
+          ],
+        },
+        primaryShade: { light: 6, dark: 5 },
+        lineHeight: 1.5,
+        fontFamily:
+          "Inter var, Inter, system-ui, Noto Sans JP, IBM Plex Sans Thai, sans-serif",
+        headings: {
+          fontFamily:
+            "SoraVariable, Sora, InterVariable, Inter, system-ui, Noto Sans JP, IBM Plex Sans Thai, sans-serif",
+          fontWeight: 800,
+        },
+        other: {
+          transition: "0.3s cubic-bezier(.19,.73,.37,.93)",
+          setAppColorScheme,
+          toggleAppColorScheme,
+          dimmed: colorScheme === "dark" ? "#8E97AD" : "#868e96",
+        },
+      }}
+    >
+      {children}
+    </MantineProvider>
+  );
+}
+
+export default MantineTheme;
--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { getCookie, setCookie } from "cookies-next";
-import { MantineProvider, ColorScheme } from "@mantine/core";
+import { ColorScheme } from "@mantine/core";
 import { NotificationsProvider } from "@mantine/notifications";
 import "@fontsource/sora/400.css";
 import "@fontsource/sora/500.css";
@@ -25,7 +25,7 @@ import { withAuthUser } from "next-firebase-auth";
 import { initAuthentication } from "services/firebase/authentication";
 import { UserProvider } from "services/firebase/user";
 import DayjsProvider from "services/libraries/dayjs";
-import { emotionCache } from "services/libraries/emotion";
+import MantineTheme from "components/MantineTheme";
 initAuthentication();
 
 function MakoTools({
@@ -43,7 +43,7 @@ function MakoTools({
   // @ts-ignore / im too lazy to do this shit
   const getLayout = Component.getLayout || ((page: any) => page);
 
-  const setAppColorScheme = (value: any) => {
+  const setAppColorScheme = (value: "light" | "dark") => {
     setStateColorScheme(value);
     setCookie("color-scheme", value, {
       maxAge: 60 * 60 * 24 * 30,
@@ -53,15 +53,6 @@ function MakoTools({
   const toggleAppColorScheme = () => {
     setAppColorScheme(colorScheme === "light" ? "dark" : "light");
   };
-
-  const getDimmed = (theme: any) => {
-    return theme.colorScheme === "dark"
-      ? theme.colors.dark[2]
-      : theme.colors.gray[6];
-  };
-
-  const getColor = (theme: any, color: any) =>
-    theme.colors[color][theme.colorScheme === "dark" ? 5 : 7];
 
   useEffect(() => {
     const handleStart = (url: any) =>
@@ -80,79 +71,7 @@ function MakoTools({
   }, [router.asPath, router.events]);
 
   return (
-    <MantineProvider
-      emotionCache={emotionCache}
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme,
-        components: {
-          NavLink: {
-            styles: (theme) => ({
-              root: {
-                "& > *:last-child": {
-                  margin: 0,
-                },
-              },
-            }),
-          },
-        },
-        colors: {
-          dark: [
-            "#D3D6E0",
-            "#AAB1C2",
-            "#8E97AD",
-            "#5F6982",
-            "#3A4259",
-            "#2C3347",
-            "#212736",
-            "#191C27",
-            "#171921",
-            "#12141C",
-          ],
-          lightblue: [
-            "#e7f5ff",
-            "#d0ebff",
-            "#a5d8ff",
-            "#74c0fc",
-            "#4dabf7",
-            "#339af0",
-            "#228be6",
-            "#1c7ed6",
-            "#1971c2",
-            "#1864ab",
-          ],
-          blue: [
-            "#edf2ff",
-            "#dbe4ff",
-            "#bac8ff",
-            "#91a7ff",
-            "#748ffc",
-            "#5c7cfa",
-            "#4c6ef5",
-            "#4263eb",
-            "#3b5bdb",
-            "#364fc7",
-          ],
-        },
-        primaryShade: { light: 6, dark: 5 },
-        lineHeight: 1.5,
-        fontFamily:
-          "Inter var, Inter, system-ui, Noto Sans JP, IBM Plex Sans Thai, sans-serif",
-        headings: {
-          fontFamily:
-            "SoraVariable, Sora, InterVariable, Inter, system-ui, Noto Sans JP, IBM Plex Sans Thai, sans-serif",
-          fontWeight: 800,
-        },
-        other: {
-          transition: "0.3s cubic-bezier(.19,.73,.37,.93)",
-          setAppColorScheme,
-          toggleAppColorScheme,
-          getDimmed,
-          getColor,
-        },
-      }}
-    >
+    <>
       <NavigationProgress />
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
@@ -177,11 +96,17 @@ function MakoTools({
           }}
         >
           <DayjsProvider>
-            {getLayout(<Component {...pageProps} />, pageProps)}
+            <MantineTheme
+              colorScheme={colorScheme}
+              setAppColorScheme={setAppColorScheme}
+              toggleAppColorScheme={toggleAppColorScheme}
+            >
+              {getLayout(<Component {...pageProps} />, pageProps)}
+            </MantineTheme>
           </DayjsProvider>
         </UserProvider>
       </NotificationsProvider>
-    </MantineProvider>
+    </>
   );
 }
 
