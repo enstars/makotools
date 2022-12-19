6f99a93cdbcab2405c493b303a84740cae971a56
neeneemi <neeneemille@gmail.com>
2022-12-18 20:29:18 -0600

    fix: 🚚 moved files

--- pages/settings/account/Username.tsx
+++ pages/settings/account/Username.tsx
@@ -2,7 +2,7 @@ import { ActionIcon, Modal, TextInput } from "@mantine/core";
 import { IconAt, IconPencil } from "@tabler/icons";
 import { useState } from "react";
 
-import DebouncedUsernameInput from "../../[user]/components/DebouncedUsernameInput";
+import DebouncedUsernameInput from "./DebouncedUsernameInput";
 
 import useUser from "services/firebase/user";
 
