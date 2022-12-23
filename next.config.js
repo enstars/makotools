10ebde948e7eb5cc7d276fedba6cd2912019f89e
yuuukun <58155530+kamishirorui@users.noreply.github.com>
2022-12-23 23:03:20 +0700

    refactor: 📦️ added bundle size viewer

--- next.config.js
+++ next.config.js
@@ -1,38 +1,43 @@
 const nextTranslate = require("next-translate");
-
-module.exports = nextTranslate({
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.svg$/i,
-        type: "asset",
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
-        use: ["@svgr/webpack"],
-      }
-    );
-    config.resolve.alias["cldr$"] = "cldrjs";
-    config.resolve.alias["cldr"] = "cldrjs/dist/cldr";
-
-    return config;
-  },
-  images: {
-    domains: ["uchuu.ensemble.moe", "assets.enstars.link"],
-  },
-
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-
-  async redirects() {
-    return [
-      {
-        source: "/issues/form",
-        destination: "https://forms.gle/W2oLnbUeTBJm647R9",
-        permanent: false,
-      },
-    ];
-  },
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+
+module.exports = withBundleAnalyzer(
+  nextTranslate({
+    webpack: (config) => {
+      config.module.rules.push(
+        {
+          test: /\.svg$/i,
+          type: "asset",
+          resourceQuery: /url/, // *.svg?url
+        },
+        {
+          test: /\.svg$/i,
+          issuer: /\.[jt]sx?$/,
+          resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+          use: ["@svgr/webpack"],
+        }
+      );
+      config.resolve.alias["cldr$"] = "cldrjs";
+      config.resolve.alias["cldr"] = "cldrjs/dist/cldr";
+
+      return config;
+    },
+    images: {
+      domains: ["uchuu.ensemble.moe", "assets.enstars.link"],
+    },
+
+    pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+
+    async redirects() {
+      return [
+        {
+          source: "/issues/form",
+          destination: "https://forms.gle/W2oLnbUeTBJm647R9",
+          permanent: false,
+        },
+      ];
+    },
+  })
+);
