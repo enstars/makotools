18427bfa6eefeb7e798c55fafda099ba9c042980
yuu <58155530+kagiura@users.noreply.github.com>
2024-05-04 01:08:54 +0800

    fix: 🩹 update card page to use FSS hook + searchoptions component

--- pages/cards/index.page.tsx
+++ pages/cards/index.page.tsx
@@ -9,12 +9,18 @@ import {
   MultiSelect,
   Input,
   Chip,
+  Tooltip,
+  ActionIcon,
+  Switch,
 } from "@mantine/core";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useDebouncedValue, useLocalStorage } from "@mantine/hooks";
-import fuzzysort from "fuzzysort";
+import { useLocalStorage } from "@mantine/hooks";
 import useTranslation from "next-translate/useTranslation";
-import { IconArrowsSort } from "@tabler/icons-react";
+import {
+  IconArrowsSort,
+  IconSortAscending,
+  IconSortDescending,
+} from "@tabler/icons-react";
 
 import NewCollectionModal from "./components/NewCollectionModal";
 
@@ -29,24 +35,7 @@ import { useCollections } from "services/makotools/collection";
 import SearchOptions from "components/core/SearchOptions";
 import useFSSList from "services/makotools/search";
 
-type SortOption = "id" | "character";
-
-interface CardViewOptions {
-  filterRarity: CardRarity[];
-  filterCharacters: string[];
-  sortOption: SortOption;
-  searchQuery: string;
-  sortDescending: boolean;
-}
-
 const CARD_LIST_INITIAL_COUNT = 20;
-const CARD_VIEW_OPTIONS_DEFAULT: CardViewOptions = {
-  filterRarity: [5],
-  filterCharacters: [],
-  sortOption: "id",
-  searchQuery: "",
-  sortDescending: false,
-};
 
 const defaultView = {
   filters: {
@@ -72,78 +61,27 @@ function Page({
     () => charactersQuery.data,
     [charactersQuery.data]
   );
-
   const { t } = useTranslation("cards");
   const theme = useMantineTheme();
   const [count, setCount] = useState<number>(CARD_LIST_INITIAL_COUNT);
-  const [cardsList, setCardsList] = useState<GameCard[]>([]);
   const [slicedCardsList, setSlicedCardsList] = useState<GameCard[]>([]);
-  const [viewOptions, setViewOptions] = useLocalStorage<CardViewOptions>({
-    key: "cardFilters",
-    defaultValue: CARD_VIEW_OPTIONS_DEFAULT,
-  });
   const [cardOptions, setCardOptions] = useLocalStorage({
     key: "cardOptions",
     defaultValue: {
       showFullInfo: false,
     },
   });
-  const [search, setSearch] = useState("");
   const [newCollectionModalOpened, setNewCollectionModalOpened] =
     useState<boolean>(false);
-  const [debouncedSearch] = useDebouncedValue(search, 200);
   const { collections, onEditCollection, onNewCollection } = useCollections();
 
-  let characterIDtoSort: { [key: number]: number } = {};
-  characters.forEach((c) => {
-    characterIDtoSort[c.character_id] = c.sort_id;
-  });
-
-  const descendingNum = viewOptions.sortDescending ? -1 : 1;
-  const SORT_FUNCTIONS = {
-    id: (a: any, b: any) => (a.id - b.id) * descendingNum,
-    character: (a: any, b: any) =>
-      (characterIDtoSort[a.character_id] - characterIDtoSort[b.character_id]) *
-      descendingNum,
-  };
-
-  useEffect(() => {
-    let filteredList: GameCard[] = cards
-      .filter((c) => {
-        return c.id <= 9999;
-      })
-      .filter((c) => {
-        return viewOptions.filterRarity.includes(c.rarity);
-      })
-      .filter((c) => {
-        if (viewOptions.filterCharacters.length)
-          return viewOptions.filterCharacters.includes(
-            c.character_id.toString()
-          );
-        return true;
-      });
-
-    const searchedList = fuzzysort.go(debouncedSearch, filteredList, {
-      keys: ["title.0", "title.1", "title.2"],
-      all: true,
+  const characterIDtoSort = useMemo<{ [key: number]: number }>(() => {
+    let result: { [key: number]: number } = {};
+    characters.forEach((c) => {
+      result[c.character_id] = c.sort_id;
     });
-    const sortedList = [...searchedList]
-      .sort((a: any, b: any) => b.score - a.score)
-      .map((cr) => cr.obj)
-      .sort(SORT_FUNCTIONS["id"])
-      .sort(SORT_FUNCTIONS[viewOptions.sortOption]);
-    setCardsList(sortedList);
-    setCount(CARD_LIST_INITIAL_COUNT);
-  }, [viewOptions, debouncedSearch]);
-
-  useEffect(() => {
-    setSlicedCardsList(cardsList.slice(0, count));
-  }, [cardsList, count]);
-
-  const loadMore = () => {
-    const newCount = count + CARD_LIST_INITIAL_COUNT;
-    setCount(newCount);
-  };
+    return result;
+  }, [characters]);
 
   const fssOptions = useMemo<FSSOptions<GameCard, typeof defaultView.filters>>(
     () => ({
@@ -184,7 +122,7 @@ function Page({
       },
       defaultView,
     }),
-    []
+    [characterIDtoSort, t]
   );
 
   const { results, view, setView } = useFSSList<
@@ -192,132 +130,18 @@ function Page({
     typeof defaultView.filters
   >(cards, fssOptions);
 
+  useEffect(() => {
+    setSlicedCardsList(results.slice(0, count));
+  }, [results, count]);
+
+  const loadMore = () => {
+    const newCount = count + CARD_LIST_INITIAL_COUNT;
+    setCount(newCount);
+  };
+
   return (
     <>
       <PageTitle title={t("title")} />
-
-      {/* <Paper mb="sm" p="md" withBorder>
-        <Text weight="700" size="xs" color="dimmed">
-          <IconSearch size="1em" /> {t("common:search.searchOptions")}
-        </Text>
-        <Group sx={{ alignItems: "flex-start" }}>
-          <TextInput
-            label={t("common:search.searchLabel")}
-            placeholder={t("search.searchPlaceholder")}
-            value={search}
-            onChange={(event) => {
-              setSearch(event.target.value);
-            }}
-            sx={{ maxWidth: 200 }}
-            variant="default"
-            icon={<IconSearch size="1em" />}
-          />
-          <Select
-            label={t("common:search.sortLabel")}
-            placeholder={t("search.sortPlaceholder")}
-            data={SORT_OPTIONS}
-            value={viewOptions.sortOption}
-            onChange={(val: SortOption) => {
-              if (val) setViewOptions({ ...viewOptions, sortOption: val });
-            }}
-            sx={{ maxWidth: 200 }}
-            variant="default"
-            icon={<IconArrowsSort size="1em" />}
-            rightSection={
-              <Tooltip label={t("common:search.sortTooltip")}>
-                <ActionIcon
-                  onClick={() => {
-                    setViewOptions((v) => ({
-                      ...viewOptions,
-                      sortDescending: !v.sortDescending,
-                    }));
-                  }}
-                  variant="light"
-                  color={theme.primaryColor}
-                >
-                  {viewOptions.sortDescending ? (
-                    <IconSortAscending size={16} />
-                  ) : (
-                    <IconSortDescending size={16} />
-                  )}
-                </ActionIcon>
-              </Tooltip>
-            }
-          />
-          <MultiSelect
-            label={t("search.charLabel")}
-            placeholder={t("search.charPlaceholder")}
-            data={characters
-              .sort(
-                (a: any, b: any) =>
-                  characterIDtoSort[a.character_id] -
-                  characterIDtoSort[b.character_id]
-              )
-              .map((c) => {
-                return {
-                  value: c.character_id.toString(),
-                  label: c.first_name[0],
-                };
-              })}
-            value={viewOptions.filterCharacters}
-            onChange={(val) => {
-              setViewOptions({ ...viewOptions, filterCharacters: val });
-            }}
-            sx={{ width: "100%", maxWidth: 400 }}
-            variant="default"
-            searchable
-          />
-          <Input.Wrapper id="rarity" label={t("search.rarityLabel")}>
-            <Chip.Group
-              multiple
-              value={viewOptions.filterRarity.map((v) => v.toString())}
-              onChange={(value) => {
-                const filterRarity = value.map(
-                  (v) => parseInt(v, 10) as CardRarity
-                );
-                setViewOptions({ ...viewOptions, filterRarity });
-              }}
-              spacing={3}
-            >
-              {[1, 2, 3, 4, 5].map((r) => (
-                <Chip
-                  key={r}
-                  value={r.toString()}
-                  radius="md"
-                  styles={{
-                    label: { paddingLeft: 10, paddingRight: 10 },
-                    iconWrapper: { display: "none" },
-                  }}
-                  color="yellow"
-                  variant="filled"
-                >
-                  {r}
-                </Chip>
-              ))}
-            </Chip.Group>
-          </Input.Wrapper>
-        </Group>
-        <Group mt="xs">
-          <Switch
-            label={t("search.showFullInfo")}
-            checked={cardOptions.showFullInfo}
-            onChange={(event) =>
-              setCardOptions({
-                ...cardOptions,
-                showFullInfo: event.currentTarget.checked,
-              })
-            }
-          />
-          <Button
-            compact
-            onClick={() => {
-              setViewOptions(CARD_VIEW_OPTIONS_DEFAULT);
-            }}
-          >
-            {t("common:search.resetFilters")}
-          </Button>
-        </Group>
-      </Paper> */}
       <SearchOptions
         searchProps={{
           placeholder: t("search.searchPlaceholder"),
@@ -335,7 +159,7 @@ function Page({
             <Select
               label={t("common:search.sortLabel")}
               placeholder={t("search.sortPlaceholder")}
-              onChange={(val: SortOption) => {
+              onChange={(val: string) => {
                 if (val) {
                   setView((v) => ({
                     ...v,
@@ -354,12 +178,40 @@ function Page({
                     disabled: true,
                     data: [
                       {
-                        label: t("common:search.sortLabel"),
+                        label: t("common:search.relevance"),
+                        value: "relevance",
                       },
                     ],
+                    value: "relevance",
+                    rightSection: undefined,
                   }
                 : {
                     disabled: false,
+                    data: fssOptions.sorts,
+                    value: view.sort.type,
+                    rightSection: (
+                      <Tooltip label="Toggle ascending/descending">
+                        <ActionIcon
+                          onClick={() => {
+                            setView((v) => ({
+                              ...v,
+                              sort: {
+                                ...v.sort,
+                                ascending: !v.sort.ascending,
+                              },
+                            }));
+                          }}
+                          variant="light"
+                          color={theme.primaryColor}
+                        >
+                          {view.sort.ascending ? (
+                            <IconSortAscending size={16} />
+                          ) : (
+                            <IconSortDescending size={16} />
+                          )}
+                        </ActionIcon>
+                      </Tooltip>
+                    ),
                   })}
             />
             <MultiSelect
@@ -377,9 +229,19 @@ function Page({
                     label: c.first_name[0],
                   };
                 })}
-              value={viewOptions.filterCharacters}
+              value={
+                view.filters.characters.length
+                  ? view.filters.characters.map((v) => v.toString())
+                  : []
+              }
               onChange={(val) => {
-                setViewOptions({ ...viewOptions, filterCharacters: val });
+                setView((v) => ({
+                  ...v,
+                  filters: {
+                    ...v.filters,
+                    characters: val.map((v) => parseInt(v, 10)),
+                  },
+                }));
               }}
               sx={{ width: "100%", maxWidth: 400 }}
               variant="default"
@@ -388,12 +250,18 @@ function Page({
             <Input.Wrapper id="rarity" label={t("search.rarityLabel")}>
               <Chip.Group
                 multiple
-                value={viewOptions.filterRarity.map((v) => v.toString())}
+                value={view.filters.rarity?.map((v) => v.toString()) || []}
                 onChange={(value) => {
                   const filterRarity = value.map(
                     (v) => parseInt(v, 10) as CardRarity
                   );
-                  setViewOptions({ ...viewOptions, filterRarity });
+                  setView((v) => ({
+                    ...v,
+                    filters: {
+                      ...v.filters,
+                      rarity: filterRarity,
+                    },
+                  }));
                 }}
                 spacing={3}
               >
@@ -416,16 +284,30 @@ function Page({
             </Input.Wrapper>
           </Group>
         }
+        display={
+          <Group>
+            <Switch
+              label={t("search.showFullInfo")}
+              checked={cardOptions.showFullInfo}
+              onChange={(event) =>
+                setCardOptions({
+                  ...cardOptions,
+                  showFullInfo: event.currentTarget.checked,
+                })
+              }
+            />
+          </Group>
+        }
       />
       {slicedCardsList.length ? (
         <>
           <Text color="dimmed" mt="xl" mb="sm" size="sm">
-            {t("resultsFound", { count: cardsList.length })}
+            {t("resultsFound", { count: results.length })}
           </Text>
           <InfiniteScroll
             dataLength={slicedCardsList.length}
             next={loadMore}
-            hasMore={count < cardsList.length}
+            hasMore={count < results.length}
             loader={
               <Center sx={{ gridColumn: "s/e" }} my="lg">
                 <Loader variant="bars" />
